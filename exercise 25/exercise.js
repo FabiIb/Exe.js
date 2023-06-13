87b2de500a604ef3e7a3e2e670ed55c2fe670b02
FabiIb <131887709+FabiIb@users.noreply.github.com>
2023-06-13 22:47:57 +0200

    exe 25
    js

---
+++ exercise 25/exercise.js
@@ -0,0 +1,10 @@
+
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+let mergeNumber = [...numberStore, newNumber]
+console.log(mergeNumber)
+
+//numberStore.push(newNumber);
+//console.log(numberStore);
+
+
