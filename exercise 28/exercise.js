5c44386fd82caf0e7c991e3328139910981d0d4e
FabiIb <131887709+FabiIb@users.noreply.github.com>
2023-06-18 18:02:10 +0200

    fix exe.js

--- exercise 28/exercise.js
+++ exercise 28/exercise.js
@@ -1,8 +1,6 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+function sum(...number) {
+  return number.reduce((a, b) => a + b);
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(...numbers));
-console.log(sum.apply(null, numbers));
-//console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(numbers[0], numbers[1], numbers[2]));
